35655c958abdcb88030f949455cd0ccff54f111a
Andre Luiz Morais <andre.luiz@setrasolucoes.com.br>
2024-12-12 15:19:09 -0300

    first commit

---
+++ api/entrypoint-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+npm install -y &&
+## npm run build -y &&
+npm run start:debug
---
+++ app-client/entrypoint-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Iniciando o script entrypoint-dev.sh"
+
+# Instala as dependências do Node.js usando npm
+echo "Instalando dependências do Node.js..."
+npm install
+
+# Inicia o servidor de desenvolvimento
+echo "Iniciando servidor HTTP de desenvolvimento..."
+npm run dev
